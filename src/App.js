272143e110823498347695813cfc59a89ab1c19e
Annderson Rey <anndersonrey@gmail.com>
2023-05-19 16:27:14 +1000

    Create complete and delete task

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { TaskCreator } from "./components/TaskCreator";
 import { TaskFilter } from "./components/TaskFilter";
@@ -6,14 +7,42 @@ import { TaskItem } from "./components/TaskItem";
 import { TaskList } from "./components/TaskList";
 
 function App() {
+  const defaultTask = [
+    { text: "Make bed", completed: false },
+    { text: "Make breakfast", completed: false },
+    { text: "Take a shower", completed: true },
+  ];
+
+  const [tasks, setTask] = React.useState(defaultTask);
+
+  const completeTask = (text) => {
+    const newTasks = [...tasks];
+    const taskIndex = newTasks.findIndex((task) => task.text === text);
+    newTasks[taskIndex].completed = true;
+    setTask(newTasks);
+  };
+
+  const deleteTask = (text) => {
+    const newTasks = [...tasks];
+    const taskIndex = newTasks.findIndex((task) => task.text === text);
+    newTasks.splice(taskIndex, 1);
+    setTask(newTasks);
+  };
+
   return (
     <>
       <TaskHeader />
       <TaskFilter />
       <TaskList>
-        <TaskItem text={"Make bed "} />
-        <TaskItem text={"Make breakfast "} />
-        <TaskItem text={"Take a shower "} />
+        {tasks.map((task) => (
+          <TaskItem
+            key={task.text}
+            text={task.text}
+            completed={task.completed}
+            onComplete={() => completeTask(task.text)}
+            onDelete={() => deleteTask(task.text)}
+          />
+        ))}
       </TaskList>
       <TaskCreator />
     </>
--- src/components/TaskItem/index.js
+++ src/components/TaskItem/index.js
@@ -1,11 +1,18 @@
 import "./style.css";
 
-function TaskItem( {text} ) {
+function TaskItem({ text, completed, onComplete, onDelete }) {
   return (
     <li className="task-item">
-      <button className="item-check"></button>
-      <p>{text}</p>
-      <button className="item-close">X</button>
+      <button
+        onClick={onComplete}
+        className={`item-check ${completed && "item-check--active"}`}
+      ></button>
+      <p className={`item-tetxt  ${completed && "item-tetxt--active"}`}>
+        {text}
+      </p>
+      <button
+        onClick={onDelete}
+        className="item-close">X</button>
     </li>
   );
 }
